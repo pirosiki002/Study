f8375a84f9af38590e3411bda000cc7b57f42991
Pirosiki_ <considerate.growth.family@gmail.com>
2023-08-27 18:13:27 +0900

    【TypeScript学習】thisの動き（Win初のUP）

---
+++ TypeScript/func_arrow_this.js
@@ -0,0 +1,16 @@
+// let Counter = function() {
+//     // 現在のthisを退避
+//     let _this = this;
+//     _this.count = 0;
+//     // count property ++ 1000mm sec per
+//     setInterval(function(){
+//         _this.count ++;
+//     },1000);
+// };
+var Counter = function () {
+    // 現在 の this を 退避 
+    var _this = this;
+    _this.count = 0;
+    // 1000 ミリ 秒 間隔 で count プロパティ を インクリメント 
+    setInterval(function () { _this.count++; }, 1000);
+};
