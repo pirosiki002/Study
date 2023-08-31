d4b64d6c13adc621221921245752e5ac94ab875d
SI <>
2023-08-31 14:33:06 +0900

    add Form from command line

---
+++ weather-app/react-typescript-weather-app/src/components/Form.tsx
@@ -0,0 +1,12 @@
+// Form.tsx
+
+const Form = () =>{
+  return (
+    <form>
+      <input type="text" name="city" placeholder="urban name"/>
+      <button type="submit">Get Weather</button>
+    </form>
+  );
+};
+
+export default Form;
