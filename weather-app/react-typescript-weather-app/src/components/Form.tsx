4377a5237d6c2d24ae53fac4ae8486a543093b8b
SI <>
2023-08-31 16:45:27 +0900

    lets make Form.tsx No.3

--- weather-app/react-typescript-weather-app/src/components/Form.tsx
+++ weather-app/react-typescript-weather-app/src/components/Form.tsx
@@ -1,14 +1,18 @@
 // Form.tsx
-
 import {useState} from 'react';
 
 const Form = () =>{
   const [city, setCity] = useState<string>("");
+  const getWeather = (e:any) => {
+    e.preventDefault();
+    fetch("")
+    .then(res => res.json())
+    .then(data => console.log(data));
+  };
   return (
     <form>
       <input type="text" name="city" placeholder="urban name" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
