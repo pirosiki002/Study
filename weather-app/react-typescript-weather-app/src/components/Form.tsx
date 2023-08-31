acee046d71cb824cbcfe7e3375dc2956d2323f3d
SI <>
2023-08-31 16:16:35 +0900

    lets make Form.tsx

--- weather-app/react-typescript-weather-app/src/components/Form.tsx
+++ weather-app/react-typescript-weather-app/src/components/Form.tsx
@@ -3,7 +3,7 @@
 import {useState} from 'react';
 
 const Form = () =>{
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState<string>("");
   return (
     <form>
       <input type="text" name="city" placeholder="urban name" onChange={e => setCity(e.target.value)}/>
