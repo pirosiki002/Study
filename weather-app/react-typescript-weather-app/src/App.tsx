b7ce795df218a9384a98e66621b4efeb712cb6a6
SI <>
2023-08-31 13:59:24 +0900

    commit from command line

--- weather-app/react-typescript-weather-app/src/App.tsx
+++ weather-app/react-typescript-weather-app/src/App.tsx
@@ -1,9 +1,10 @@
+import Title from "./components/Title";
 import './App.css';
 
 function App() {
   return (
     <div className="test">
-      <h1>Hello React world!!!</h1>
+      <Title />
     </div>
   );
 }
