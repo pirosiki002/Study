7c0a13de5a51fc706bd44cf7a6d0894db14f3bb7
SI <>
2023-09-15 08:00:07 +0900

    ✨ Unst TS Sec2-10 type check

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -1,1 +1,7 @@
-console.log("code add here!!!!");
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+var num1 = "5";
+var num2 = 2.8;
+var result = add(num1, num2);
+console.log(result);
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -1,1 +1,9 @@
-console.log("code add here!!!!");
+const add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+const num1 = 5;
+const num2 = 2.8;
+
+const result = add(num1, num2);
+console.log(result);
