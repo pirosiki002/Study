ab33ea0e0c3de50c540e1e6c692282637a964b84
SI <>
2023-09-15 08:36:18 +0900

    ✨ Unst TS Sec2-13 output string+number

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -1,11 +1,20 @@
-var add = function (n1, n2) {
+var add = function (n1, n2, showResult, phrase) {
     // console.log(typeof n1);
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //   console.log(`n1 or n2 is not type of number`);
     // }
-    return n1 + n2;
+    var result = n1 + n2;
+    if (showResult) {
+        // console.log(phrase + n1 + n2);
+        console.log(phrase + result);
+    }
+    else {
+        // return n1 + n2;
+        return result;
+    }
 };
 var num1 = 5;
 var num2 = 2.8;
-var result = add(num1, num2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result";
+add(num1, num2, printResult, resultPhrase);
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -1,13 +1,17 @@
-const add = (n1: number, n2: number) => {
-  // console.log(typeof n1);
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   console.log(`n1 or n2 is not type of number`);
-  // }
-  return n1 + n2;
+const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
+  const result = n1 + n2;
+  if (showResult) {
+    // console.log(phrase + n1 + n2);
+    console.log(phrase + result);
+  } else {
+    // return n1 + n2;
+    return result;
+  }
 };
 
 const num1 = 5;
 const num2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result";
 
-const result = add(num1, num2);
-console.log(result);
+add(num1, num2, printResult, resultPhrase);
