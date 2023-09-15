43933acf2454dd241017d689cfc84fa172056cc3
SI <>
2023-09-15 10:20:29 +0900

    ✨ Unst TS Sec2-19 Enum

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -1,7 +1,17 @@
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "piro",
     age: 41,
     hobbies: ["English", "Programing"],
+    role: Role.ADMIN,
 };
 var favoAct;
 favoAct = ["sport"];
@@ -9,4 +19,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log("Auth user");
 }
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -1,7 +1,18 @@
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR = 200,
+}
+
 const person = {
   name: "piro",
   age: 41,
   hobbies: ["English", "Programing"],
+  role: Role.ADMIN,
 };
 
 let favoAct: string[];
@@ -10,5 +21,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
-  // console.log(hobby.map());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("Auth user");
 }
