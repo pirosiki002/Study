ebb9cb5d80095cf3747571a7e5326645e73aab46
SI <>
2023-09-15 10:00:06 +0900

    ✨ Unst TS array type

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -1,9 +1,12 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
 var person = {
     name: "piro",
     age: 41,
+    hobbies: ["English", "Programing"],
 };
+var favoAct;
+favoAct = ["sport"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -1,6 +1,14 @@
 const person = {
   name: "piro",
   age: 41,
+  hobbies: ["English", "Programing"],
 };
 
+let favoAct: string[];
+favoAct = ["sport"];
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map());
+}
