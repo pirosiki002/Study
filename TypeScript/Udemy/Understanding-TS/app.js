c3a9f0dc07659f68095ec300ba5d82790672beaa
SI <>
2023-09-16 04:51:07 +0900

    ✨ Unst TS Sec2-22 Typeof Literal

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -1,14 +1,22 @@
-var combine = function (input1, input2) {
+var combine = function (input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === "as number") {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
 };
-var cobineAges = combine(30, 26);
+var cobineAges = combine(30, 26, "as text");
 console.log(cobineAges);
-var combinedNames = combine("Max", "Annna");
+var combinedStringAges = combine("30", "26", "as text");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Annna", "as text");
 console.log(combinedNames);
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -1,15 +1,31 @@
-const combine = (input1: number | string, input2: number | string) => {
+const combine = (
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as number" | "as text"
+) => {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+
   return result;
+  // if (resultConversion === "as number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 };
 
-const cobineAges = combine(30, 26);
+const cobineAges = combine(30, 26, "as number");
 console.log(cobineAges);
 
-const combinedNames = combine("Max", "Annna");
+const combinedStringAges = combine("30", "26", "as text");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Annna", "as text");
 console.log(combinedNames);
