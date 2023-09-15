c9b4bc8a0886856a068288f753b1fa51bca4e094
SI <>
2023-09-16 07:55:59 +0900

    ✨ Unst TS Sec2-24 typeof Function

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -4,4 +4,9 @@ var add = function (n1, n2) {
 var printResult = function (num) {
     console.log("Result:" + num);
 };
-console.log(printResult(add(5, 12)));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
+printResult(add(5, 12));
