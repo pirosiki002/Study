0f5c2237997b53e9211dcc5554a19edaa79d4850
SI <>
2023-09-16 08:16:52 +0900

    Unst TS 2-27 typeof function & callback

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -4,6 +4,13 @@ var add = function (n1, n2) {
 var printResult = function (num) {
     console.log("Result:" + num);
 };
+var addAndHandle = function (n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+};
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -6,6 +6,15 @@ const printResult = (num: number): void => {
   console.log("Result:" + num);
 };
 
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+  const result = n1 + n2;
+  cb(result);
+};
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
+
 let combineValues: (a: number, b: number) => number;
 combineValues = add;
 // combineValues = printResult;
