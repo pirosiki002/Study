1815b3339c0a96a160f6f37169d6bb08f114cdd7
SI <>
2023-09-17 09:24:57 +0900

    ✨ TS study 2-29 typeof never

--- TypeScript/Udemy/Understanding-TS/app.js
+++ TypeScript/Udemy/Understanding-TS/app.js
@@ -1,19 +1,11 @@
-var add = function (n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+var generateError = function (message, code) {
+    throw { message: message, errorCode: code };
 };
-var printResult = function (num) {
-    console.log("Result:" + num);
-};
-var addAndHandle = function (n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-};
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-printResult(add(5, 12));
+generateError("error occored", 500);
--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -7,3 +7,11 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+const generateError = (message: string, code: number): never => {
+  throw { message: message, errorCode: code };
+  // while(true);
+};
+
+const result = generateError("error occored", 500);
+console.log(result);
