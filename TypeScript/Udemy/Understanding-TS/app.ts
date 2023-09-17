2deb7aef507287e9b064baca6e4be2fd6c876d37
SI <>
2023-09-17 09:07:21 +0900

    ✨ TS study 2-28 unknown type

--- TypeScript/Udemy/Understanding-TS/app.ts
+++ TypeScript/Udemy/Understanding-TS/app.ts
@@ -1,25 +1,9 @@
-const add = (n1: number, n2: number) => {
-  return n1 + n2;
-};
+let userInput: unknown;
+let userName: string;
 
-const printResult = (num: number): void => {
-  console.log("Result:" + num);
-};
+userInput = 5;
+userInput = "Max";
 
-const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
-  const result = n1 + n2;
-  cb(result);
-};
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
-
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-printResult(add(5, 12));
+if (typeof userInput === "string") {
+  userName = userInput;
+}
