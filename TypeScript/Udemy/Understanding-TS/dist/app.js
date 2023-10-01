1109175b43f257d770e7308c29a534dc2ac3133e
SI <>
2023-10-01 09:57:09 +0900

    [TSstudy]Sec3-43 remain

--- TypeScript/Udemy/Understanding-TS/dist/app.js
+++ TypeScript/Udemy/Understanding-TS/dist/app.js
@@ -1,6 +1,11 @@
 "use strict";
-let userName = 'Max';
 const button = document.querySelector("button");
+const add = (n1, n2) => {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+};
 const clickHandler = (message) => {
     console.log("ClickHandler clicked!!!" + message);
 };
