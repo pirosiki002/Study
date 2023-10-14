161c63a32b7a0dd55fc92456860e548e7d507229
SI <>
2023-10-14 09:04:59 +0900

    :sparkles:[TSStudy]Destructuring .js, map

--- TypeScript/Udemy/Understanding-TS/dist/app.js
+++ TypeScript/Udemy/Understanding-TS/dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-const userName = 'Max';
-let age = 30;
-age = 29;
+const add = (a, b = 1) => a + b;
+const printOutput = output => {
+    console.log(output);
+};
+printOutput(add(2));
 const button = document.querySelector('button');
 if (button) {
     button.addEventListener('click', event => {
@@ -13,15 +15,9 @@ const activeHobbies = ['Hikingggg'];
 activeHobbies.push(...hobbies);
 console.log(activeHobbies);
 const person = {
-    name: 'max',
+    firstName: 'Max',
     age: 30
 };
-const copiedPerson = Object.assign({}, person);
-const add = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
 //# sourceMappingURL=app.js.map
