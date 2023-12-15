1c2764b435e4f3fd93bbbb837fea48f26f9c39be
SI <>
2023-12-15 09:45:54 +0900

    feat: LearnNextChap15Login

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -2,6 +2,19 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -12,11 +25,17 @@ export const { auth, signIn, signOut } = NextAuth({
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
         if (parsedCredentials.success) {
-          // Add your logic here to validate the credentials and return the user object
-          // For example, you might fetch the user from your database and compare the hashed password
-          // If the credentials are valid, return the user object
-          // If the credentials are invalid, return null
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) {
+            return user;
+          }
         }
+
+        console.log('Invalid credentials');
         return null;
       },
     }),
