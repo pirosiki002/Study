b8d2214ca9312230eeebea62bda38f7510d12d19
SI <>
2024-01-02 09:54:05 +0900

    feat: Chapter10:Server and Client

--- React-fundation/app/page.js
+++ React-fundation/app/page.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import LikeButton from './like-button.js';
 
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
@@ -6,11 +6,6 @@ function Header({ title }) {
 
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'MargaretHamilton'];
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
 
   return (
     <div>
@@ -20,8 +15,7 @@ export default function HomePage() {
           <li key="{name}">{name}</li>
         ))}
       </ul>
-
-      <button onClick="{handleClick}">Like ({likes})</button>
+      <LikeButton />
     </div>
   );
 }
