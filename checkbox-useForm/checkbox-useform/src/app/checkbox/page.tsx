68ce3d2a4328fa2d795f35cd0e220b5552adee58
SI <>
2024-03-16 09:53:10 +0900

    feat: 1. 静的にフォームをコーディング

--- checkbox-useForm/checkbox-useform/src/app/checkbox/page.tsx
+++ checkbox-useForm/checkbox-useform/src/app/checkbox/page.tsx
@@ -1,11 +1,60 @@
-import React from "react";
+// ref:https://tech.andpad.co.jp/entry/2022/12/22/100000
+"use client";
+import type { NextPage } from "next";
+import { useState } from "react";
 
-const CheckBox = () => {
+interface MyFavoriteFoods {
+  id: string;
+  name: string;
+  checked: boolean;
+  disabled: boolean;
+}
+
+const MultipleCheckBox: NextPage = () => {
+  const [MyFavoriteFoods, setMyFavoriteFoods] = useState<MyFavoriteFoods[]>([
+    {
+      id: "sushi",
+      name: "寿司",
+      checked: false,
+      disabled: false,
+    },
+    {
+      id: "yakiniku",
+      name: "焼肉",
+      checked: false,
+      disabled: false,
+    },
+    {
+      id: "khao_mangai",
+      name: "カオマンガイ",
+      checked: false,
+      disabled: false,
+    },
+  ]);
+
   return (
     <>
-      <p>test</p>
+      <form>
+        <div>
+          {MyFavoriteFoods.map((item) => {
+            return (
+              <div key={item.id}>
+                <input
+                  id={item.id}
+                  type="checkbox"
+                  defaultChecked={item.checked}
+                  disabled={item.disabled}
+                ></input>
+                <label htmlFor={item.id}>{item.name}</label>
+                {/* <label>{item.name}</label> */}
+              </div>
+            );
+          })}
+        </div>
+        <button type="submit">[Submit]</button>
+      </form>
     </>
   );
 };
 
-export default CheckBox;
+export default MultipleCheckBox;
