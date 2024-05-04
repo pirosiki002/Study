343625eb8c694044282cacc3b68557f33cec0ea3
SI <>
2024-05-05 06:11:37 +0900

    feat: arrow function

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/020_arrow_function/start/main.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,15 @@
 function fn(number) {
-  return number * 2;
+  return number * 3;
 }
+
+console.log(fn(2));
+// const fnArrow = (number) => {
+//   return number * 2;
+// };
+
+const fnArrow = (number) => number * 3;
+
+const fnArrowObj = (number) => ({ result: number * 2 });
+
+// console.log(fnArrow(2));
+console.log(fnArrowObj(2));
