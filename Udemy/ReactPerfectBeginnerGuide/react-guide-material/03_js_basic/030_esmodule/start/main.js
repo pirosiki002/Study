9bd38e6cd1748ae76c2e35fac71eb69b23084bc3
SI <>
2024-05-05 06:37:14 +0900

    feat: sec3 react and JS arrow func import export

---
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,11 @@
+import functionB, { hello, User } from "./module.js";
+// import { hello, User } from "./module.js";
+// import { User } from "./module.js";
+
+// import funcB from "./module.js";
+// import functionB from "./module.js";
+
+hello();
+const user = new User("Tom");
+user.hello();
+functionB();
