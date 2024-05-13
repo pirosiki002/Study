826b0eee4310839a31a3aa3eda5f5d858523e3da
Pirosiki_ <considerate.growth.family@gmail.com>
2024-05-14 05:33:23 +0900

    feat: eventlistener

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/045_eventlistener/start/main.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/045_eventlistener/start/main.js
@@ -3,3 +3,16 @@ console.dir(h1Element);
 console.log(h1Element.textContent);
 
 h1Element.textContent = 'changed title';
+
+const btnEl = document.querySelector('button');
+// btnEl.addEventListener('click', (e) => {
+//     console.dir(e.target.textContent);
+//     console.log('hello')
+// });
+
+const helloFn = (e) => {
+    console.dir(e.target.textContent);
+    console.log('good evening!');
+}
+
+btnEl.addEventListener('click', helloFn);
