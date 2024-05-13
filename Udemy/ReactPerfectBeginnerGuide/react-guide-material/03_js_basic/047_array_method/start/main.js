6d1b1d0bba3f77c837ff0583c1fcb63017298617
Pirosiki_ <considerate.growth.family@gmail.com>
2024-05-14 05:46:47 +0900

    feat: map1

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/047_array_method/start/main.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,16 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+for(i = 0; i < arry.length; i++){
+    newArry.push(arry[i] * 2);
+}
+
+console.log(newArry);
+
+// const newArry2 = arry.map(val => val * 3);
+const newArry2 = arry.map((val, i, arry) => {
+    console.log(val, i, arry);
+    return val * 2;
+})
+
+console.log(newArry2);
