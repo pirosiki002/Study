011e82eaa49666168166349f282959c9e665b82f
Pirosiki_ <considerate.growth.family@gmail.com>
2024-05-21 06:18:28 +0900

    feat: map and filter

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/047_array_method/start/main.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/047_array_method/start/main.js
@@ -1,16 +1,20 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
 
-for(i = 0; i < arry.length; i++){
-    newArry.push(arry[i] * 2);
+for(let i=0; i < arry.length; i++){
+    const val = arry[i] * 2;
+    if(val > 50){
+        newArry.push(val);
+    }
 }
 
 console.log(newArry);
 
-// const newArry2 = arry.map(val => val * 3);
-const newArry2 = arry.map((val, i, arry) => {
-    console.log(val, i, arry);
-    return val * 2;
-})
+// const newArry2 = arry.map(val => val * 2);
 
+const newArry2 = arry.map(val => val * 2).filter(val => val > 50);
 console.log(newArry2);
+
+// const newArry3 = newArry2.filter(val => val > 50);
+
+// console.log(newArry3);
