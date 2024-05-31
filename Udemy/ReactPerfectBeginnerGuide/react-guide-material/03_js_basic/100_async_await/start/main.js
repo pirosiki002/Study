165c8fc946d96ffd405a3ab85de0f0f55df44401
Pirosiki_ <considerate.growth.family@gmail.com>
2024-06-01 05:14:36 +0900

    feat: async_await

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/100_async_await/start/main.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/03_js_basic/100_async_await/start/main.js
@@ -1,17 +1,33 @@
 // 非同期処理（Promise）
 let a = 0;
 
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        a = 1;
-        resolve(a)
-    }, 2000);
-}).then((b) => {
-    console.log(b);
-    return b;
-}).then((b) => {
-    console.log(b);
-}).catch((c) => {
-    console.log('catchが実行', c)
-})
+init();
+async function init(){
+    try{
+
+        const result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                a = 1;
+                // resolve(a)
+                reject(a);
+            }, 2000);
+        })
+    }catch(e){
+        console.log('catch done', e);
+    }
+
+    console.log(a);
+    // console.log(result);
+    
+}
+
+// }).then((b) => {
+//     console.log(b);
+//     return b;
+// }).then((b) => {
+//     console.log(b);
+// }).catch((c) => {
+//     console.log('catchが実行', c)
+// })
+
 
