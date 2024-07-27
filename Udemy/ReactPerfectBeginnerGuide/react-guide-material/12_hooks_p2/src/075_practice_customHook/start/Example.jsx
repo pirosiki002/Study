8c41da6f111fdc4110d5f081f583ef919c78fc47
SI <>
2024-07-27 09:52:15 +0900

    feat: No135 before creat a custom hook

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/075_practice_customHook/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/075_practice_customHook/start/Example.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [count, setCount] = useState(0);
+
+  onclick = (num) => {
+    const addCnt = count + 1;
+    setCount(addCnt);
+  };
   return (
     <>
       <h3>練習問題</h3>
@@ -6,7 +14,7 @@ const Example = () => {
         記述を変更し、完成コードと同じ状態になるようにしてください。
         startフォルダの中にhooks.jsというファイルを作成しその中でuseCountというカスタムフックを作成してください。
       </p>
-      <div>Counts: {}</div>
+      <div>Counts: {count}</div>
       <button onClick={() => {}}>Count Up!</button>
     </>
   );
