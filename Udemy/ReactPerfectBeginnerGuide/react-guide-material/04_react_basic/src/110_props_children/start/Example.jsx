6e3edd84b0053a77596ae5738104d4166d0d5900
SI <>
2024-08-03 11:28:52 +0900

    feat: props children

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/110_props_children/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/110_props_children/start/Example.jsx
@@ -1,15 +1,28 @@
 import Profile from "./components/Profile";
 import Container from "./components/Container";
+import { Children } from "react";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      {/* <Container title="Childrenとは？" /> */}
+      <Container
+        title="Childrenとは？"
+        children={[
+          <Profile key={profile[0].name} {...profile[0]} />,
+          <Profile key={profile[1].name} {...profile[1]} />,
+        ]}
+        first={<Profile {...profile[0]} />}
+        second={<Profile {...profile[1]} />}
+      />
+      {/* <Profile {...profile[0]} />
+        <Profile {...profile[1]} /> */}
+      {/* </Container> */}
     </div>
   );
 };
