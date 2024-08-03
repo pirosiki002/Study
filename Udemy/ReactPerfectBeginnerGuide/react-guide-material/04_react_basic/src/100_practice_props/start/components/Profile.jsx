96064f42822e48d0f5b0b8d7aa85952539077119
SI <>
2024-08-03 10:55:04 +0900

    feat: 100practice my answer

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/100_practice_props/start/components/Profile.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/100_practice_props/start/components/Profile.jsx
@@ -1,7 +1,16 @@
 import "./Profile.css";
 
 const Profile = (props) => {
-  return <div className="profile"></div>;
+  const { name, age, country } = props;
+  return (
+    <>
+      <div className="profile">
+        <div className="name">Name:{name}</div>
+        <div className="age">Age:{age}</div>
+        <div className="country">From:{country}</div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
