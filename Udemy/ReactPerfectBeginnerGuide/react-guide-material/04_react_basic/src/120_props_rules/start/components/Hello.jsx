e59e6a59eb92462c705a41536f03b4cb97d06b15
Pirosiki_ <considerate.growth.family@gmail.com>
2024-08-10 05:55:36 +0900

    feat: props readonly

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/120_props_rules/start/components/Hello.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/120_props_rules/start/components/Hello.jsx
@@ -1,7 +1,12 @@
 const Hello = (props) => {
+  // const name = "Tom";
+  // props.name = "Bob";
+  const desc = Reflect.getOwnPropertyDescriptor(props, "name");
+  console.log(desc);
   return (
     <div>
       <h3>Hello {props.name}</h3>
+      {/* <h3>Hello {name}</h3> */}
     </div>
   );
 };
