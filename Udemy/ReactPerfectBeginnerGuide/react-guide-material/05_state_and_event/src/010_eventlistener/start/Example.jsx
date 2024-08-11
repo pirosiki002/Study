4f74ef3cb5768a96b003ef4e2ebbe2ab6ed82ca0
Pirosiki_ <considerate.growth.family@gmail.com>
2024-08-12 06:34:18 +0900

    feat: event handler

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,34 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert(`button was clicked`);
+  };
+
+  const hello = () => "hello react";
+  // console.log(hello);
   return (
-    <></>
+    <>
+      {/* <button onClick={clickHandler}>click me</button> */}
+      <button
+        onClick={() => {
+          clickHandler();
+        }}
+      >
+        {/* <button
+        onClick={() => {
+          clickHandler;
+        }}
+      > */}
+        click me
+      </button>
+      <button
+        onClick={() => {
+          alert(`button was clicked`);
+        }}
+      >
+        clickme2
+      </button>
+      {hello}
+    </>
   );
 };
 
