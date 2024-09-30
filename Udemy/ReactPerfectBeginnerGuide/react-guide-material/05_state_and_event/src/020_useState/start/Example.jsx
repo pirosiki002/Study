dd5f5af10ad2f40d020f70d6e7957cfb20510384
SI <>
2024-10-01 08:42:18 +0900

    feat: useState

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/020_useState/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/020_useState/start/Example.jsx
@@ -1,8 +1,20 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  // let valArray = useState();
+  let [val, setVal] = useState();
+  // console.log(valArray);
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          // const setFn = valArray[1];
+          setVal(e.target.value);
+          // valArray = e.target.value;
+        }}
+      />
+      = {val}
     </>
   );
 };
