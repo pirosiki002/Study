0d3c7e9e54a0d5ff76404f1d8ccca34c48c2b4ba
SI <>
2024-10-15 08:36:07 +0900

    feat: useState button cnt

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+// const [buttonC, setButtonC] = useState(0);
+
 const Example = () => {
-  
+  const [buttonA, setButtonA] = useState(0);
+  const [buttonB, setButtonB] = useState(10);
+  // const [buttonC, setButtonC] = useState(0);
+
+  // if (1) {
+  //   const [buttonC, setButtonC] = useState(0);
+  // }
+
+  // const buttonACnt = 0;
+  // const buttonBCnt = 0;
+  // const buttonCCnt = 0;
+
+  // const handleA = () => {
+  //   setButtonA(buttonA + 1);
+  // };
+
+  return (
+    <>
+      <p>ボタンAを{buttonA}回押しました！</p>
+      {/* <button onClick={handleA}>ボタンA</button> */}
+      <button
+        onClick={() => {
+          setButtonA(buttonA + 1);
+        }}
+      >
+        ボタンA
+      </button>
+      <p>ボタンBを{buttonB}回押しました！</p>
+      <button
+        onClick={() => {
+          setButtonB(buttonB + 1);
+        }}
+      >
+        ボタンB
+      </button>
+      {/* <p>ボタンCを{buttonC}回押しました！</p> */}
+    </>
+  );
 };
 
 export default Example;
