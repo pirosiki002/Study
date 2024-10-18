aa286cd77ee2b847ab4924a9b65ac1e7d2d6af2b
Pirosiki_ <considerate.growth.family@gmail.com>
2024-10-19 06:33:15 +0900

    feat: useState control timing

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/050_prev_state/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  const [cnt, setCnt] = useState(0);
+
+  const countDown = () => {
+    setCnt(cnt - 1);
+  };
+
+  return (
+    <>
+      <p>現在のカウント数：{cnt}</p>
+      <button
+        // キャメルケースで指定しないと反応しません！！
+        // onclick={() => {
+        onClick={() => {
+          setCnt(cnt + 1);
+          setCnt((prevstate) => {
+            return prevstate + 1;
+          });
+          console.log(cnt);
+        }}
+      >
+        +
+      </button>
+      {/* <button
+        onClick={() => {
+          setCnt(cnt - 1);
+        }}
+      > */}
+      <button onClick={countDown}>-</button>
+    </>
+  );
 };
 
 export default Example;
