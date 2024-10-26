e8aa3e03ed17406423ea4ae07725a03ccc3b87a2
SI <>
2024-10-26 09:32:23 +0900

    feat: array useState management

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  console.log(nums);
+
+  const handleSuffle = () => {
+    const newNums = [...nums];
+    const value = newNums.pop();
+    console.log(value);
+    console.log(newNums);
+
+    newNums.unshift(value);
+    // console.log(newNums);
+    setNums(newNums);
+  };
 
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={handleSuffle}>shuffle</button>
     </>
   );
 };
