e6e144cd6f91bc870f7e3687ec27ab32506835e2
SI <>
2024-10-29 09:26:37 +0900

    feat: component management

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/070_state_and_component/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/05_state_and_event/src/070_state_and_component/start/Example.jsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
 
 const Example = () => {
+  const [toggle, setToggle] = useState(true);
+
+  const changeToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
+  return (
+    <>
+      <button onClick={changeToggle}>Toggle</button>
+      {toggle ? <Count key="A" name={"A"} /> : <Count key="B" name={"B"} />}
+    </>
+  );
+};
+
+const Count = (props) => {
+  const { name } = props;
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
@@ -10,7 +26,9 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>
+        {name}: {count}
+      </h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
