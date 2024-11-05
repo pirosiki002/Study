7f95c488c62f875800aa084bf84d01e3afd09616
SI <>
2024-11-05 10:01:14 +0900

    feat: react map disp

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,14 +1,21 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  // const animalList = [];
+  // for (const animal of animals) {
+  //   animalList.push(<li>{animal}</li>);
+  // }
+
+  // const helloAnimals = animals.map((animal) => <li key={1}>Hello{animal}</li>);
 
   return (
     <>
       <h3>配列の操作</h3>
-      <ul>
-        
-      </ul>
+      {/* <ul>{animalList}</ul> */}
+      {/* <ul>{helloAnimals}</ul> */}
+      {animals.map((animal) => {
+        return <li key={1}>hello, {animal}</li>;
+      })}
     </>
   );
 };
