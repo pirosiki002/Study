fbf05cec7e76be3420b784686d083926c01ec3da
SI <>
2024-11-05 10:57:36 +0900

    feat: profileList map disp

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/030_practice_list/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/030_practice_list/start/Example.jsx
@@ -1,11 +1,39 @@
+import Profile from "./components/Profile";
+
+// const profileList = [{ name: "Geo", age: 18, hobbies: ["sports", "music"] }];
+const profileList = [
+  { name: "Geo", age: 18, hobbies: ["sports", "music"] },
+  { name: "Tom", age: 25, hobbies: ["movie", "music"] },
+  { name: "Lisa", age: 21, hobbies: ["sports", "travel", "game"] },
+];
+
 const Example = () => {
   return (
     <>
-      <h3>練習問題</h3>
-      <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
-      <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
+      {/* <h3>練習問題</h3>
+      <p>
+        Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。
+      </p>
+      <p>
+        また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。
+      </p> */}
+      {/* <Profile
+        name={profileList[0].name}
+        age={profileList[0].age}
+        hobbies={profileList[0].hobbies}
+      /> */}
       <ul>
-        {/* ここに記述 */}
+        {profileList.map((profile) => (
+          // <Profile
+          //   key={profile.name}
+          //   name={profile.name}
+          //   age={profile.age}
+          //   hobbies={profile.hobbies}
+          // />
+          <li key={profile.name}>
+            <Profile {...profile} />
+          </li>
+        ))}
       </ul>
     </>
   );
