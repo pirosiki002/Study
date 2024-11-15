816e563c4b9887fdb51c7d29e05c7144a36ef41c
Pirosiki_ <considerate.growth.family@gmail.com>
2024-11-16 07:21:28 +0900

    feat: if sentense etc

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,40 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
+            console.log(animalStr.indexOf(filterVal));
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          // .map((animal) => (
+          //   <li key={animal}>{animal}</li>
+          // ))}
+          // if文
+          // .map((animal) => {
+          //   if (animal === "Dog") {
+          //     return <li key={animal}>{animal + "★"}</li>;
+          //   } else {
+          //     return <li key={animal}>{animal}</li>;
+          //   }
+          // })}
+          // 三項演算子?
+          // .map((animal) => {
+          //   return (
+          //     <li key={animal}>{animal === "Dog" ? animal + "★" : animal}</li>
+          //   );
+          // })}
+          .map((animal) => {
+            // return <li key={animal}>{animal + (animal === "Dog" && "★")}</li>;
+            const animalStr = animal ?? "nullかundefinedです。";
+
+            return (
+              <li key={animalStr}>
+                {animalStr}
+                {animalStr === "Dog" && "★"}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
