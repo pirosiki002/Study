4c645c31e9b2694e4c5bab17a3d498503ea4d962
Pirosiki_ <considerate.growth.family@gmail.com>
2024-12-21 06:12:15 +0900

    feat: environment create

---
+++ Udemy/TailwindCSS/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: [],
+  content: ["./public/**/*.{html,js}"], // HTMLファイルのパスを指定
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
