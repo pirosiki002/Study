b920e72d8c7a817bcf0d382ebed78d42095864f5
SI <>
2025-01-21 08:08:04 +0900

    radio button

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/090_radio/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/090_radio/start/Example.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [fruit, setFruit] = useState("");
+
+  const handleChange = (e) => setFruit(e.target.value);
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <p style={{ textAlign: "center" }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+      <label>
+        <input
+          type="radio"
+          value="Apple"
+          checked={fruit === "Apple"}
+          onChange={handleChange}
+        />
+        Apple
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Banana"
+          checked={fruit === "Banana"}
+          onChange={handleChange}
+        />
+        Banana
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Cherry"
+          checked={fruit === "Cherry"}
+          onChange={handleChange}
+        />
+        Cherry
+      </label>
+      <h3>I'd like to eat a {fruit}</h3>
+    </>
   );
 };
 
