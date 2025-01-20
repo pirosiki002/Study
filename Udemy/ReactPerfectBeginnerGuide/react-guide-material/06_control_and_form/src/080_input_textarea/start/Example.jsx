7bec3a87ae4debc847f4c66acaf086bb0898d2e6
SI <>
2025-01-21 07:46:56 +0900

    Form how to creat input and textarea

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] = useState("");
+
+  const handleClear = () => {
+    setVal("");
+  };
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <p style={{ textAlign: "center" }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+      <label htmlFor="456">label</label>
+      <input
+        id="123"
+        placeholder="hello"
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+        value={val}
+      />
+      <textarea
+        id="456"
+        placeholder="hello"
+        value={val}
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+      <h3>{val}</h3>
+      <button onClick={handleClear}>clear</button>
+    </>
   );
 };
 
