d23f47a16c91507ddb1b43beb74ab059a436fa53
Pirosiki_ <considerate.growth.family@gmail.com>
2025-01-25 07:16:32 +0900

    useQuery standard

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/000_useQuery/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/000_useQuery/start/Example.jsx
@@ -1,11 +1,17 @@
 import User from "./User";
+// import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div style={{ margin: "2em" }}>
-      <h1>ユーザ情報</h1>
-      <User />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div style={{ margin: "2em" }}>
+        <h1>ユーザ情報</h1>
+        <User />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/000_useQuery/start/User.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/04_react_basic/src/000_useQuery/start/User.jsx
@@ -1,27 +1,48 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 // URLからユーザーデータ取得
 const fetchUsers = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!res.ok) throw new Error("Failed to fetch");
   return res.json();
 };
 
 function User() {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
 
   // useEffectでfetchデータ取得
-  useEffect(() => {
-    fetchUsers().then((data) => {
-      console.log({ data });
-      setUsers(data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetchUsers().then((data) => {
+  //     console.log({ data });
+  //     setUsers(data);
+  //   });
+  // }, []);
+  // const result = useQuery("users", fetchUsers);
+  // console.log(result);
+
+  const { data, error, isLoading, isSuccess, status } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  console.log({ status });
+  console.log({ isLoading });
+  console.log({ isSuccess });
+  console.log({ error });
 
   return (
     <div>
       <h2>ユーザ一覧</h2>
       <div>
-        {users.map((user) => (
+        {/* {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))} */}
+        {data?.map((user) => (
           <div key={user.id}>{user.name}</div>
         ))}
       </div>
