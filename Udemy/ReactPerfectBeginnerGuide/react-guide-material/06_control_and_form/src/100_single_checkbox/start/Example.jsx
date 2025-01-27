293330826c4e2300df39e16194a230b4db94de65
Pirosiki_ <considerate.growth.family@gmail.com>
2025-01-28 05:17:36 +0900

    checkbox mycode

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/100_single_checkbox/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/100_single_checkbox/start/Example.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [text, setText] = useState("OFF!");
+
+  const handleClick = () => {
+    const newIsChecked = !isChecked;
+    setIsChecked(newIsChecked);
+    if (newIsChecked) {
+      setText("ON");
+    } else {
+      setText("OFF");
+    }
+  };
+
   return (
     <p style={{ textAlign: "center" }}>
       startフォルダの内容が表示されます。
       <br />
       練習用に使ってください！
+      <label htmlFor="my-check">check</label>
+      <input
+        type="checkbox"
+        id="my-check"
+        checked={isChecked}
+        onClick={handleClick}
+      ></input>
+      {text}
     </p>
   );
 };
