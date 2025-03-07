fb31054d92469594fe57939f3aaa50f2b443fd46
Pirosiki_ <considerate.growth.family@gmail.com>
2025-03-08 06:17:07 +0900

    feat: searchParams practice

--- nextjs-dashboard/app/posts/[slug]/comments/page.tsx
+++ nextjs-dashboard/app/posts/[slug]/comments/page.tsx
@@ -1,3 +1,19 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>Post: {params.slug}</div>;
+type SearchParamsValue = string | string[] | undefined;
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    sort?: SearchParamsValue;
+    id?: SearchParamsValue;
+  }>;
+}) {
+  const { sort, id } = await searchParams;
+  // http://localhost:3000/posts/exe-post/comments?id=3&sort=res
+  return (
+    <>
+      {sort}
+      {id}
+    </>
+  );
 }
