3832c82799984c4905ecb7ea53d8085dba324e39
Pirosiki_ <considerate.growth.family@gmail.com>
2025-03-15 06:58:20 +0900

    feat: not found

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,10 +1,18 @@
 import { setTimeout } from 'node:timers/promises';
 import { Suspense } from 'react';
 import { Clock } from './clock';
+import { notFound } from "next/navigation";
 
 export const dynamic = 'force-dynamic';
 
-export default function Page() {
+export default async function Page() {
+
+  // const user = await fetchUser("122");
+  const user = await fetchUser("123");
+  if (!user) {
+    return notFound();
+  }
+
   return (
     <div>
       <h1>Streaming SSR</h1>
@@ -19,4 +27,12 @@ export default function Page() {
 async function LazyComponent() {
   await setTimeout(3000);
   return <p>Lazy Component</p>;
+}
+
+async function fetchUser(id: string) {
+  if (id === "123") {
+    return { id: "123", name: "Taro" };
+  } else {
+    return null;
+  }
 }
