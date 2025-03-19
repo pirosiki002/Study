568d296832ddf9c947d91b1c5d4e85914d58a83b
Pirosiki_ <considerate.growth.family@gmail.com>
2025-03-20 06:09:58 +0900

    feat: useMemo example

--- nextjs-dashboard/app/useMemo/page.client.tsx
+++ nextjs-dashboard/app/useMemo/page.client.tsx
@@ -1,12 +1,21 @@
 'use client';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 export default function ClientPage() {
   const [a, setA] = useState(1);
   const [b, setB] = useState(2);
 
   // a, b どちらが呼ばれても再描写される
   // a * bが複雑な処理だった場合に負荷がかかる
-  const result = a * b;
+  // const result = a * b;
+
+  const result = useMemo(() => {
+    console.log('useMemo called!!!');
+    return a * b;
+  }, [a, b]);
+
+  const handleDummy = () => {
+    console.log('handleDummy called!!');
+  };
 
   return (
     <div>
@@ -15,6 +24,7 @@ export default function ClientPage() {
       </p>
       <button onClick={() => setA(a + 1)}>aを増やす</button>
       <button onClick={() => setB(b + 1)}>bを増やす</button>
+      <button onClick={handleDummy}>click here</button>
     </div>
   );
 }
