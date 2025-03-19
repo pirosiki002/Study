c036d3e8260ed42976403953abab6982c7800644
Pirosiki_ <considerate.growth.family@gmail.com>
2025-03-20 05:49:00 +0900

    feat: usecallback compare

--- nextjs-dashboard/app/useCallback/page.client.tsx
+++ nextjs-dashboard/app/useCallback/page.client.tsx
@@ -7,15 +7,21 @@ const ClientPage: FC = () => {
   //   const handleCount = () => {
   //     setCount(count + 1);
   //   };
+  console.log('ClientPage called');
 
   const handleCount = useCallback(() => {
     setCount(count + 1);
   }, [count]);
 
+  const handleLog = useCallback(() => {
+    console.log('Button clicked!');
+  }, []); // 依存配列が空なので、関数は再生成されません
+
   return (
     <>
-      <button onClick={handleCount}>click here!</button>
-      <p>current count:{count}</p>
+      <button onClick={handleCount}>Count up!</button>
+      <p>current count: {count}</p>
+      <button onClick={handleLog}>Log message</button>
     </>
   );
 };
