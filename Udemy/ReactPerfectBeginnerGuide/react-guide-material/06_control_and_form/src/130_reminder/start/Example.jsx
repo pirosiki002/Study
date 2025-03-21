eeda68200a616dd6f3bf4c7bc176c13ed088a78c
Pirosiki_ <considerate.growth.family@gmail.com>
2025-03-22 06:20:52 +0900

    feat: todo list (list only)

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/130_reminder/start/Example.jsx
@@ -1,17 +1,12 @@
+import Todo from "./Todo";
 const Example = () => {
-  const onDelete = (value) => {
-    // do delete
-  };
-
   // This code is making now!!!
-
   return (
     <>
       <h3>完成系と同じような機能を作成してください。</h3>
       <p>また、枠線で囲まれた部品をコンポーネントに分離してください。</p>
       <h2>Reminder</h2>
-      {/* List */}
-      <button onClick={onDelete(e.target.value)}>完了</button>
+      <Todo />
     </>
   );
 };
--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/130_reminder/start/Todo.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/06_control_and_form/src/130_reminder/start/Todo.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
+const todosList = [
+  {
+    id: 1,
+    content: "店予約する",
+  },
+  {
+    id: 2,
+    content: "卵買う",
+  },
+  {
+    id: 3,
+    content: "郵便出す",
+  },
+];
+
 const Todo = () => {
-  const todosList = [
-    {
-      id: 1,
-      content: "店予約する",
-    },
-    {
-      id: 2,
-      content: "卵買う",
-    },
-    {
-      id: 3,
-      content: "郵便出す",
-    },
-  ];
+  const [list, setList] = useState(todosList);
+
+  const onDelete = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
+
+  return (
+    <>
+      {/* List.jsに移動する */}
+      {list.map((todo) => (
+        <div key={todo.id} style={{ display: "flex", alignItems: "center" }}>
+          <button onClick={() => onDelete(todo.id)}>完了</button>
+          <p style={{ margin: 0 }}>{todo.content}</p>
+        </div>
+      ))}
+    </>
+  );
 };
+export default Todo;
