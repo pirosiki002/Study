afc16669fb1af31b35d5e45e5c83dc64721c0e44
Pirosiki_ <considerate.growth.family@gmail.com>
2025-05-06 07:53:48 +0900

    useEffect practice

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.jsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+
 const Example = () => {
-  return <></>;
+  const [time, setTime] = useState(0);
+
+  useEffect(
+    () => {
+      console.log("useEffect is called");
+      const intervalId = window.setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+
+      // クリーンアップ関数を返す
+      return () => {
+        clearInterval(intervalId);
+      };
+    },
+    // time,
+    // setTime
+    []
+  );
+
+  return (
+    <>
+      <h3>
+        <time>{time}</time>
+        <span>sec past</span>
+      </h3>
+    </>
+  );
 };
 
 export default Example;
