9be52882130e61adbde22342a80ba6347f343a65
Pirosiki_ <considerate.growth.family@gmail.com>
2025-05-06 08:07:52 +0900

    update useEffect

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/020_useEffect_update/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/020_useEffect_update/start/Example.jsx
@@ -4,18 +4,24 @@ const Example = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
+    console.log("useEffect is called");
     window.setInterval(() => {
-      setTime(prev => prev + 1);
+      setTime((prev) => prev + 1);
     }, 1000);
-  }, [])
-  
+  }, []);
+
+  useEffect(() => {
+    // console.log("useEffect updated");
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key", time);
+  }, [time]);
+
   return (
     <h3>
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
-    );
+  );
 };
 
 export default Example;
