52332ede0754fb7a26c4c8d42edd1785668d5a9c
Pirosiki_ <considerate.growth.family@gmail.com>
2025-05-10 07:06:00 +0900

    feat: useeffect clean up

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
@@ -1,18 +1,39 @@
 import { useEffect, useState } from "react";
 
 const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
+  return (
+    <>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp((prev) => !prev)}>Toggle</button>
+    </>
+  );
+};
+
+const Timer = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
-    window.setInterval(() => {
-      setTime(prev => prev + 1);
+    console.log("timer mounted");
+    // let intervalId = null;
+    const intervalId = window.setInterval(() => {
+      console.log("interval called");
+      setTime((prev) => prev + 1);
     }, 1000);
-  }, [])
-  
+    return () => {
+      // console.log("end");
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
   useEffect(() => {
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
+    // console.log("updated");
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key-end", time);
+    return () => {
+      // debugger;
+      // console.log("updated end");
+    };
   }, [time]);
 
   return (
@@ -20,7 +41,7 @@ const Example = () => {
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
-    );
+  );
 };
 
 export default Example;
