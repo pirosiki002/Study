70e7c78d8c0bb2b632c501bd57280e7d170d550a
Pirosiki_ <considerate.growth.family@gmail.com>
2025-05-17 06:58:49 +0900

    fix 10_redux with AI agent

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/13_redux/end/src/010_redux_no_rtk/components/CounterButton.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/13_redux/end/src/010_redux_no_rtk/components/CounterButton.jsx
@@ -1,15 +1,25 @@
 import { useDispatch } from "react-redux";
 // 使用していないCounterContextのimport削除
 
-const CounterButton = ({calcType, step}) => {
+const CounterButton = ({ calcType, step }) => {
+  const dispatch = useDispatch();
+  // const dispatch = useCounterDispatch();
 
-    const dispatch = useDispatch();
-    // const dispatch = useCounterDispatch();
+  const clickHandler = () => {
+    // 元のコード: calcTypeをそのままアクションタイプとして使用
+    // dispatch({ type: calcType, step });
 
-    const clickHandler = () => {
-        dispatch({ type: calcType, step });
-    }
+    // 修正: calcTypeを適切なアクションタイプに変換
+    // Reduxのアクションタイプは定数として定義するのがベストプラクティス
+    const actionType = calcType === "+" ? "INCREMENT" : "DECREMENT";
+    dispatch({ type: actionType, step });
+  };
 
-    return <button onClick={clickHandler}>{calcType}{step}</button>
-}
+  return (
+    <button onClick={clickHandler}>
+      {calcType}
+      {step}
+    </button>
+  );
+};
 export default CounterButton;
