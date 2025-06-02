9b0058ab4d2beb2fa55a5bb5e72e1d913d2f8787
Pirosiki_ <considerate.growth.family@gmail.com>
2025-06-03 07:26:25 +0900

    fix: add id

--- todo_app_with_redux/src/actions/index.js
+++ todo_app_with_redux/src/actions/index.js
@@ -2,9 +2,12 @@
 export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 
+let nextTodoId = 0;
+
 // Action Creators
 export const addTodo = (text) => ({
   type: ADD_TODO,
+  id: nextTodoId++,
   text,
 });
 
