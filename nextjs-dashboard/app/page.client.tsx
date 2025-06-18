cecfe8b17e774ec62774f4a99ab3778976105258
pirosiki002 <considerate.growth.family@gmail.com>
2025-06-19 07:12:05 +0900

    fix: page client base

--- nextjs-dashboard/app/page.client.tsx
+++ nextjs-dashboard/app/page.client.tsx
@@ -1,14 +1,9 @@
 'use client';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
 
 export const ClientPage = () => {
-  const currentUrl = usePathname();
   return (
     <>
-      <p>current url = {currentUrl}</p>
-      {/* <Link href={'/urltest'}>move to urltest page</Link> */}
-      <Link href="/urltest?page=55&category=teacher">move to urltest page</Link>
+      <p>page client</p>
     </>
   );
 };
--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,52 +1,10 @@
-import { setTimeout } from 'node:timers/promises';
-import { Suspense } from 'react';
-import { Clock } from './clock';
-import { permanentRedirect, redirect } from 'next/navigation';
 export const dynamic = 'force-dynamic';
-
-// let redirected = false;
+import { ClientPage } from './page.client';
 
 export default async function Page() {
-  const team = await fetchTeam('5');
-  // if (!team && !redirected) {
-  if (!team) {
-    // redirected = true;
-    // redirect('/cookies');
-    // permanentRedirect('/cookies');
-    console.log('redirected');
-  }
-
   return (
-    <div>
-      <h1>Streaming SSR</h1>
-      <Clock />
-      <Suspense fallback={<>loading...</>}>
-        <LazyComponent />
-      </Suspense>
-    </div>
+    <>
+      <ClientPage />
+    </>
   );
-}
-
-async function fetchTeam(id: string) {
-  // 仮のチームデータ
-  const teams = [
-    { id: '1', name: 'Team Alpha' },
-    { id: '2', name: 'Team Beta' },
-  ];
-
-  // id に一致するチームを検索
-  return teams.find(team => team.id === id) || null;
-}
-
-async function LazyComponent() {
-  await setTimeout(3000);
-  return <p>Lazy Component</p>;
-}
-
-async function fetchUser(id: string) {
-  if (id === '123') {
-    return { id: '123', name: 'Taro' };
-  } else {
-    return null;
-  }
 }
