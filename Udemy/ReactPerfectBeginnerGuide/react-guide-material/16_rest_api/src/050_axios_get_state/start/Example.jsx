8b6605d51a76507bf4d06372652558683cf318a9
pirosiki002 <considerate.growth.family@gmail.com>
2025-08-19 05:38:08 +0900

    feat: getUser from db json

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/050_axios_get_state/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/050_axios_get_state/start/Example.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Example = () => {
-  return <></>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    console.log('useEffect called');
+    const getUser = async () => {
+      const res = await axios.get(' http://127.0.0.1:3003/user');
+      // console.log(res.data);
+      setUsers(res.data);
+    };
+    getUser();
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => {
+        return (
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>age:{user.age}</p>
+            <p>hobby:{user.hobbies.join(',')}</p>
+            {/* <p>hobby:{user.hobbies}</p> */}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Example;
