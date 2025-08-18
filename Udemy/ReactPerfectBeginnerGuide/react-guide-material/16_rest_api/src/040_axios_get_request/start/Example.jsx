75f9a8655146bd90155005c9723ac9f8970d2fe7
pirosiki002 <considerate.growth.family@gmail.com>
2025-08-19 05:20:04 +0900

    feat: axios

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+// request by axios
 const Example = () => {
-  return <></>;
+  // useEffect(async () => {
+  useEffect(() => {
+    // axios.get(' http://127.0.0.1:3003/user').then(res => {
+    //   console.log(res.data);
+    // });
+    const getUser = async () => {
+      const res = await axios.get(' http://127.0.0.1:3003/user');
+      console.log(res.data);
+    };
+    getUser();
+  }, []);
 };
 
 export default Example;
