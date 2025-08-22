47c9c5b64b97b996f78369e6b393decf5f581458
pirosiki002 <considerate.growth.family@gmail.com>
2025-08-23 05:35:04 +0900

    feat: update delete

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/060_other_method/start/api/todo.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/060_other_method/start/api/todo.js
@@ -2,24 +2,39 @@ import axios from 'axios';
 
 const ENDPOINT_URL = 'http://localhost:3003/todo';
 
+// IDを文字列に統一する共通関数
+const normalizeId = todo => ({
+  ...todo,
+  id: String(todo.id),
+});
+
 const todoApi = {
   async getAll() {
     const result = await axios.get(ENDPOINT_URL);
     console.log(result);
     return result.data;
   },
+
   async post(todo) {
-    const result = await axios.post(ENDPOINT_URL, todo);
+    const normalizedTodo = normalizeId(todo);
+    const result = await axios.post(ENDPOINT_URL, normalizedTodo);
     console.log(result);
     return result.data;
   },
+
   async delete(todo) {
-    const result = await axios.delete(ENDPOINT_URL + '/' + todo.id);
+    const normalizedTodo = normalizeId(todo);
+    const result = await axios.delete(ENDPOINT_URL + '/' + normalizedTodo.id);
     console.log(result);
     return result.data;
   },
+
   async patch(todo) {
-    const result = await axios.put(ENDPOINT_URL + '/' + todo.id, todo);
+    const normalizedTodo = normalizeId(todo);
+    const result = await axios.put(
+      ENDPOINT_URL + '/' + normalizedTodo.id,
+      normalizedTodo
+    );
     console.log(result);
     return result.data;
   },
--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/060_other_method/start/components/Item.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/060_other_method/start/components/Item.jsx
@@ -1,35 +1,56 @@
-import { useState } from "react";
-import { useDispatchTodos } from "../context/TodoContext";
+import { useState } from 'react';
+import { useDispatchTodos } from '../context/TodoContext';
+import todoApi from '../api/todo';
 
 const Item = ({ todo }) => {
   const [editingContent, setEditingContent] = useState(todo.content);
   const dispatch = useDispatchTodos();
 
-  const changeContent = (e) => setEditingContent(e.target.value);
+  const changeContent = e => setEditingContent(e.target.value);
 
   const toggleEditMode = () => {
     const newTodo = { ...todo, editing: !todo.editing };
-    dispatch({ type: 'todo/update', todo: newTodo });
+    todoApi.patch(newTodo).then(newTodo => {
+      dispatch({ type: 'todo/update', todo: newTodo });
+    });
   };
 
-  const confirmContent = (e) => {
+  const confirmContent = e => {
     e.preventDefault();
     const newTodo = {
       ...todo,
       editing: !todo.editing,
       content: editingContent,
     };
-    dispatch({ type: 'todo/update', todo: newTodo });
+    // dispatch({ type: 'todo/update', todo: newTodo });
+    todoApi.patch(newTodo).then(newTodo => {
+      dispatch({ type: 'todo/update', todo: newTodo });
+    });
   };
 
-  const complete = (todo) => {
-    dispatch({ type: "todo/delete", todo });
+  // const complete = todo => {
+  const complete = () => {
+    console.log('削除対象のtodo:', todo); // デバッグ用
+    console.log('todo.id:', todo.id); // IDを確認
+
+    // IDを文字列に変換
+    const todoToDelete = { ...todo, id: String(todo.id) };
+
+    todoApi
+      .delete(todo)
+      .then(() => {
+        dispatch({ type: 'todo/delete', todo: todo });
+      })
+      .catch(error => {
+        console.error('削除エラー:', error);
+      });
   };
 
   return (
     <div key={todo.id}>
-      <button onClick={() => complete(todo)}>完了</button>
-      <form onSubmit={confirmContent} style={{ display: "inline" }}>
+      {/* <button onClick={() => complete(todo)}>完了</button> */}
+      <button onClick={() => complete()}>完了</button>
+      <form onSubmit={confirmContent} style={{ display: 'inline' }}>
         {todo.editing ? (
           <input type="text" value={editingContent} onChange={changeContent} />
         ) : (
