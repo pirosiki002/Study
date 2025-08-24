e89411ab538cb35e84cba3af7cc39ed8bd068b3f
pirosiki002 <considerate.growth.family@gmail.com>
2025-08-25 05:45:55 +0900

    feat: dynamic import

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/070_dynamic_import/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/070_dynamic_import/start/Example.jsx
@@ -1,5 +1,15 @@
+import { add } from './add';
+
 const Example = () => {
-  
+  const dynamicImport = async () => {
+    const module = await import('./add');
+    console.log(module);
+  };
+
+  import('./add').then(module => {
+    console.log(module);
+  });
+  console.log(add(1, 2));
 };
 
 export default Example;
