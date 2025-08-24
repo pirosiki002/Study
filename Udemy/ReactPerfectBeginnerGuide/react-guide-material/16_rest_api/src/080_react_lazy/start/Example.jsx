3140258fa614c26558fdd4ffb645944675206a75
pirosiki002 <considerate.growth.family@gmail.com>
2025-08-25 06:05:21 +0900

    feat: dynamic import lazy etc...

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/080_react_lazy/start/Example.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/16_rest_api/src/080_react_lazy/start/Example.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
-import ComponentA from "./components/ComponentA";
+import { useState, lazy, Suspense, startTransition } from 'react';
+// import ComponentA from './components/ComponentA';
 
+const LazyComponentA = lazy(() => import('./components/ComponentA'));
+const LazyComponentB = lazy(() => import('./components/ComponentB'));
 const Example = () => {
-  const [compA, setCompA] = useState(false);
+  const [compA, setCompA] = useState(true);
 
   return (
     <>
-      <button onClick={() => setCompA((prev) => !prev)}>ComponentA</button>
-      {compA && <ComponentA />}
+      {/* <button onClick={() => setCompA(prev => !prev)}>ComponentA</button> */}
+      <button
+        onClick={() => {
+          startTransition(() => {
+            setCompA(prev => !prev);
+          });
+        }}
+      >
+        ComponentA
+      </button>
+      <Suspense fallback={<div>loading now...</div>}>
+        {compA ? <LazyComponentA /> : <LazyComponentB />}
+      </Suspense>
     </>
   );
 };
