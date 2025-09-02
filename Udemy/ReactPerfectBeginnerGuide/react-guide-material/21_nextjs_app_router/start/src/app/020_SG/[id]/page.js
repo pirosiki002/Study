d50e39e386a75f52b2b387c646a8a6d3d5756487
SI <>
2025-09-02 10:39:18 +0900

    feat/SG part1

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/app/020_SG/[id]/page.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/app/020_SG/[id]/page.js
@@ -1,6 +1,15 @@
-
+// http://localhost:4020/020_SG/1
+import { paths } from '../paths';
 export default function Page({ params }) {
-  const date = new Date;
-  return <h3>このページは{params.id}です。{date.toJSON()}</h3>;
+  const date = new Date();
+  return (
+    <h3>
+      このページは{params.id}です。{date.toJSON()}
+    </h3>
+  );
 }
-
+// name is static
+export async function generateStaticParams() {
+  // return [{ id: '1' }];
+  return paths;
+}
--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/app/030_SG_fetch/[id]/page.js
@@ -1,8 +1,18 @@
-import { ENDPOINT } from "@/constants";
-import Article from "../../../components/article";
+import { ENDPOINT } from '@/constants';
+import Article from '../../../components/article';
+
+// 静的生成用のパラメータを定義
+export async function generateStaticParams() {
+  // 実際のAPIから記事IDを取得
+  const articles = await fetch(ENDPOINT).then(res => res.json());
+
+  return articles.map(article => ({
+    id: article.id.toString(),
+  }));
+}
 
 export default async function Detail({ params }) {
-  const article = await fetch(`${ENDPOINT}/${params.id}`).then((res) =>
+  const article = await fetch(`${ENDPOINT}/${params.id}`).then(res =>
     res.json()
   );
   return (
