139e9cb57074664fbcf28a742c592523635c00b4
pirosiki002 <considerate.growth.family@gmail.com>
2025-09-10 05:56:30 +0900

    feat: api fetch

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/app/api/article/route.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/app/api/article/route.js
@@ -1,11 +1,10 @@
 // API Routes
 // https://nextjs.org/docs/app/api-reference/file-conventions/route
-import { ENDPOINT } from "@/constants";
+import { ENDPOINT } from '@/constants';
 
 export async function GET() {
-
+  const data = await fetch(ENDPOINT).then(res => res.json());
+  return Response.json(data);
 }
 
-export async function POST(request) {
-
-}
+export async function POST(request) {}
