c563d39b4eb0df9d9291aefcb493fec4950fb254
pirosiki002 <considerate.growth.family@gmail.com>
2025-09-11 05:53:30 +0900

    feat: server action

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/actions/createItem.js
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/21_nextjs_app_router/start/src/actions/createItem.js
@@ -1,6 +1,24 @@
-
-import { ENDPOINT } from "@/constants";
+'use server';
+import { ENDPOINT } from '@/constants';
 
 export async function createItem(formData) {
-  
+  const id = formData.get('id');
+  const title = formData.get('title');
+  //   console.log(id, title);
+  if (id === '' || title === '') {
+    return { msg: 'input field is empty' };
+  }
+
+  try {
+    // throw 'error';
+    const res = await fetch(ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id, title }),
+    });
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    return { msg: 'register error' };
+  }
 }
