74a7996fc0f8680dd1d45fc06d91c59474ea4782
pirosiki002 <considerate.growth.family@gmail.com>
2025-09-20 10:02:02 +0900

    task component base

--- my-storybook-app/taskbox/.storybook/preview.ts
+++ my-storybook-app/taskbox/.storybook/preview.ts
@@ -1,7 +1,10 @@
-import type { Preview } from "@storybook/react-vite";
+import type { Preview } from '@storybook/react-vite';
+import '../src/index.css';
 
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
