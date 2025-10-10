3237c03db6f1158f083e0226731f9e82a51ebe06
pirosiki002 <considerate.growth.family@gmail.com>
2025-10-11 08:02:05 +0900

    test: test userEvent

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/19_test/src/050_userEvent/start/components/Message.test.jsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/19_test/src/050_userEvent/start/components/Message.test.jsx
@@ -1,5 +1,34 @@
-// import { render, screen } from "@testing-library/react";
-// import Message from "./Message";
-// import userEvent from "@testing-library/user-event";
+import { render, screen } from '@testing-library/react';
+import Message from './Message';
+import userEvent from '@testing-library/user-event';
 
-test('ダミーテスト:削除して記載してください。', () => {});
+describe('message component check', () => {
+  describe('first display check', () => {
+    test('input string is valid?', () => {
+      render(<Message />);
+      const h2El = screen.getByRole('heading', { name: '入力された文字:' });
+      expect(h2El).toBeInTheDocument();
+    });
+  });
+
+  describe('moniter operation check', () => {
+    test('input change -> push send button', async () => {
+      const user = userEvent.setup();
+
+      render(<Message />);
+      const inputEl = screen.getByRole('textbox');
+      const btnEl = screen.getByRole('button', { name: '送信ボタン' });
+
+      const h2El = screen.getByRole('heading', { name: '入力された文字:' });
+      expect(btnEl).toBeDisabled(); // invalid button
+      expect(inputEl.value).toBe(''); // space
+
+      await user.type(inputEl, 'hello!');
+
+      expect(btnEl).not.toBeDisabled();
+      await user.click(btnEl);
+
+      expect(h2El.textContent).toBe('入力された文字:hello!');
+    });
+  });
+});
