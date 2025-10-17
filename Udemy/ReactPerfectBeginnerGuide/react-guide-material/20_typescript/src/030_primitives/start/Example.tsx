7ef8c75a87a66f679be328d4e6b4629e96232117
SI <>
2025-10-18 08:50:44 +0900

    feat: primitive types

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/030_primitives/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,28 @@
 const Example = () => {
   // 練習用
+  let str: string = 'Hello';
+  // str = 1;
+  str = 'byebye';
+  console.log(str);
+
+  const num: number = 102;
+  console.log(num);
+
+  const bignum: bigint = 103n;
+  console.log(bignum);
+
+  const bool: boolean = true;
+  console.log(bool);
+
+  // let nullish: null = null;
+  // let undefinedValue: undefined = undefined;
+
+  // literal type
+  // let trueVal: true = true;
+  // trueVal = false;
+  // let num123: 123 = 123;
+  // let strHello: 'Hello' = 'Hello';
+  // strHello = 'Hello';
 };
 
 export default Example;
