040cd842c1516765c88e2266e84462ef9fcf0e2d
SI <>
2025-10-29 08:26:04 +0900

    feat: type function

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,20 @@
 const Example = () => {
   // 練習用
+  function sum1(x: number, y?: number) {
+    console.log(y);
+    return x + y;
+  }
+
+  const sum2 = (x: number, y: number) => x + y;
+
+  // const result1 = sum1(1, '2');
+  // const result1 = sum1(1);
+  // const result1 = sum1(1, 2);
+  const result1 = sum1(1);
+
+  const result2 = sum2(3, 4);
+
+  console.log(result1, result2);
 };
 
 export default Example;
