ac66ad39925cd5a8bd45de8356ae5ddf8bf5de9a
SI <>
2025-10-29 08:15:08 +0900

    feat: type allias

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/070_type_alias/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,34 @@
 const Example = () => {
   // 練習用
+  type User = {
+    name: string;
+    age: number;
+  };
+
+  // const user: UserProfile = { name: 'taro', age: 22 };
+
+  type UserName = string;
+  type UserAge = number;
+  type UserGender = 'man' | 'woman' | 'others';
+
+  type UserProfile = {
+    name: UserName;
+    age: UserAge;
+    gender: UserGender;
+  };
+
+  const user: User = {
+    name: 'taro',
+    age: 24,
+  };
+
+  const userProfile: UserProfile = {
+    name: 'jiro',
+    age: 100,
+    gender: 'man',
+  };
+
+  console.log(user, userProfile);
 };
 
 export default Example;
