f8182f2de6011907b98c6de663f720a19e210f73
SI <>
2025-10-29 07:36:06 +0900

    feat: union type

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/040_union_types/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,12 @@
 const Example = () => {
   // 練習用
+  let strOrNumOrbool: string | number | boolean = 'Hello';
+  strOrNumOrbool = 123;
+  console.log(strOrNumOrbool);
+
+  type HelloOrNum = 'Hello' | number;
+  const hello: HelloOrNum = 'Hello';
+  console.log(hello);
 };
 
 export default Example;
