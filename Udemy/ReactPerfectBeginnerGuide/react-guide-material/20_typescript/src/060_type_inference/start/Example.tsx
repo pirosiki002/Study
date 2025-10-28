6841413c06d65f93c2e95fa65ddbf50c682c6cc8
SI <>
2025-10-29 08:07:18 +0900

    feat: type inference

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/060_type_inference/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/060_type_inference/start/Example.tsx
@@ -1,5 +1,12 @@
 const Example = () => {
   // 練習用
+  // let str = 'hello';
+  // str = 123;
+  const obj = { name: 'taro', age: 10 };
+  console.log(obj);
+
+  const num = 123;
+  console.log(num);
 };
 
 export default Example;
