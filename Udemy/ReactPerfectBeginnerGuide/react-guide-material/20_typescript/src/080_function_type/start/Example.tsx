fd99cd4153cb16ab21bd7209d19f88ec4724c1fa
SI <>
2025-11-01 08:20:07 +0900

    feat: func type

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
@@ -1,20 +1,28 @@
 const Example = () => {
   // 練習用
-  function sum1(x: number, y?: number) {
-    console.log(y);
-    return x + y;
-  }
+  // function sum1(x: number, y: number) {
+  //   console.log(y);
+  //   return x + y;
+  // }
 
-  const sum2 = (x: number, y: number) => x + y;
+  // const sum2 = (x: number, y: number): number => x + y;
+  const sum2 = (x: number, y: number): string => 'hello';
 
   // const result1 = sum1(1, '2');
   // const result1 = sum1(1);
   // const result1 = sum1(1, 2);
-  const result1 = sum1(1);
+  // const result1 = sum1(1);
 
-  const result2 = sum2(3, 4);
+  // const result2 = sum2(4, 4);
+  const result2 = sum2(4, 4);
 
-  console.log(result1, result2);
+  // console.log(result1, result2);
+  console.log(result2);
+
+  type Sum = (x: number, y: number) => number;
+  const sum3: Sum = (x, y) => x + y;
+  const result3 = sum3(10, 2);
+  console.log(result3);
 };
 
 export default Example;
