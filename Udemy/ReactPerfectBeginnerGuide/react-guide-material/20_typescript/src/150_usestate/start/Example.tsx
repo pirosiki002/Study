72c87b7f02ab19df56ace7c241cd2091422736d5
pirosiki002 <considerate.growth.family@gmail.com>
2025-11-08 06:42:18 +0900

    feat: type of useState

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/150_usestate/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useState } from 'react';
+
+type Todo = {
+  id: number;
+  text: string;
+};
+
+type User = {
+  name: string;
+  age: number;
+};
 
 const Example = () => {
-  
+  const [text, setText] = useState('hello');
+  useState(0);
+
+  const [animals, setAnimals] = useState<string[]>(['dog', 'cat']);
+  const [users, setUsers] = useState<User[]>([
+    { name: 'Tom', age: 22 },
+    { name: 'Jerry', age: 5 },
+  ]);
+  const [todos, setTodos] = useState<Todo[]>([{ id: 1, text: 'test' }]);
 };
 
 export default Example;
