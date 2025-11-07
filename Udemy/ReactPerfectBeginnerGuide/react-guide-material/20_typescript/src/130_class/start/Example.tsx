dc4b84b8407b7cff78567f3b84f543986fe8ad64
pirosiki002 <considerate.growth.family@gmail.com>
2025-11-08 06:18:39 +0900

    feat: type of class

--- Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/130_class/start/Example.tsx
+++ Udemy/ReactPerfectBeginnerGuide/react-guide-material/20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,29 @@
+// class AdminUser extends User {}
+
+class User {
+  public name: string;
+  private age: number;
+  // protected age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
 const Example = () => {
   // 練習用
+  const user = { name: 'Hanako', age: 21 };
+  const user1 = new User('Hanako', 25);
+
+  console.log(user1);
+
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </div>
+  );
 };
 
 export default Example;
