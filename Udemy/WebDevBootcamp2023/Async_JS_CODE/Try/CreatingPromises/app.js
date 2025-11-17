5e132408ad74006bda40b0a489720e06893fd201
pirosiki002 <considerate.growth.family@gmail.com>
2025-11-18 06:34:35 +0900

    feat: promise simple delay color change func

--- Udemy/WebDevBootcamp2023/Async_JS_CODE/Try/CreatingPromises/app.js
+++ Udemy/WebDevBootcamp2023/Async_JS_CODE/Try/CreatingPromises/app.js
@@ -1,25 +1,67 @@
-const fakeRequest = url => {
+// const delayedColorChange = (newColor, delay, doNext) => {
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = newColor;
+//     doNext && doNext();
+//   }, delay);
+// };
+
+// delayedColorChange('red', 1000, () => {
+//   delayedColorChange('orange', 1000, () => {
+//     delayedColorChange('yellow', 1000, () => {
+//       delayedColorChange('green', 1000, () => {
+//         delayedColorChange('blue', 1000, () => {
+//           delayedColorChange('indigo', 1000, () => {
+//             delayedColorChange('violet', 1000, () => {});
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+/**
+ * 指定した色に変更する関数
+ *
+ * @param {new color} newColor
+ * @param {delay time} delay
+ */
+const delayedColorChange = (newColor, delay) => {
   return new Promise((resolve, reject) => {
-    const rand = Math.random();
     setTimeout(() => {
-      if (rand < 0.7) {
-        resolve('this is dummy data');
-        // return;
-      } else {
-        reject('connection is timeout');
-      }
-    }, 1000);
+      document.body.style.backgroundColor = newColor;
+      resolve();
+      // doNext && doNext();
+    }, delay);
   });
 };
 
-fakeRequest('/hoge')
-  .then(data => {
-    console.log('success', data);
-  })
-  .catch(err => {
-    console.log('error', err);
-  });
+delayedColorChange('red', 1000)
+  .then(() => delayedColorChange('orange', 1000))
+  .then(() => delayedColorChange('yellow', 1000))
+  .then(() => delayedColorChange('green', 1000))
+  .then(() => delayedColorChange('blue', 1000))
+  .then(() => delayedColorChange('indigo', 1000))
+  .then(() => delayedColorChange('violet', 1000));
 
-// new Promise((resolve, reject) => {
-//   resolve();
-// });
+// delayedColorChange('red', 1000, () => {
+//   return delayedColorChange('orange', 1000);
+// })
+// delayedColorChange('red', 1000)
+//   .then(() => {
+//     return delayedColorChange('orange', 1000);
+//   })
+//   .then(() => {
+//     return delayedColorChange('yellow', 1000);
+//   })
+//   .then(() => {
+//     return delayedColorChange('green', 1000);
+//   })
+//   .then(() => {
+//     return delayedColorChange('blue', 1000);
+//   })
+//   .then(() => {
+//     return delayedColorChange('indigo', 1000);
+//   })
+//   .then(() => {
+//     return delayedColorChange('violet', 1000);
+//   });
