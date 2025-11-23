1103a1fa20a9e297721264982cdd6c852a507d22
SI <>
2025-11-23 09:20:33 +0900

    feat: add printRainbow

--- Udemy/WebDevBootcamp2023/Async_JS_CODE/Try/AsyncFunctions/app.js
+++ Udemy/WebDevBootcamp2023/Async_JS_CODE/Try/AsyncFunctions/app.js
@@ -1,22 +1,56 @@
-const login = async (username, password) => {
-  if (!username || !password) {
-    throw new Error('nothing username or password');
-  }
-  if (password === 'secret') {
-    return 'welcome!!!';
-  }
-
-  throw new Error('password is wrong');
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
 };
 
-// login('hoge', 'hogehoge')
-login('hoge', 'secret')
-  .then(msg => {
-    console.log('login success', msg);
-  })
-  .catch(err => {
-    console.log('login error', err);
-  });
+// delayedColorChange('red', 1000)
+//   .then(() => delayedColorChange('orange', 1000))
+//   .then(() => delayedColorChange('yellow', 1000))
+//   .then(() => delayedColorChange('green', 1000))
+//   .then(() => delayedColorChange('blue', 1000))
+//   .then(() => delayedColorChange('indigo', 1000))
+//   .then(() => delayedColorChange('violet', 1000));
+
+async function rainbow() {
+  await delayedColorChange('red', 1000);
+  console.log('yahoo!');
+  await delayedColorChange('orange', 1000);
+}
+
+async function printRainbow() {
+  await rainbow();
+  console.log('rainbow comp!!!');
+}
+
+printRainbow();
+
+// rainbow().then(data => {
+//   console.log('rainbow comp!', data);
+// });
+
+// const login = async (username, password) => {
+//   if (!username || !password) {
+//     throw new Error('nothing username or password');
+//   }
+//   if (password === 'secret') {
+//     return 'welcome!!!';
+//   }
+
+//   throw new Error('password is wrong');
+// };
+
+// // login('hoge', 'hogehoge')
+// login('hoge', 'secret')
+//   .then(msg => {
+//     console.log('login success', msg);
+//   })
+//   .catch(err => {
+//     console.log('login error', err);
+//   });
 
 // async function hello() {}
 
