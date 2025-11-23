00086f937648a2f81061ed3ef2fddc939fcc55a0
SI <>
2025-11-23 09:07:39 +0900

    feat: async for example

--- Udemy/WebDevBootcamp2023/Async_JS_CODE/Try/AsyncFunctions/app.js
+++ Udemy/WebDevBootcamp2023/Async_JS_CODE/Try/AsyncFunctions/app.js
@@ -1,14 +1,34 @@
-async function hello() {}
+const login = async (username, password) => {
+  if (!username || !password) {
+    throw new Error('nothing username or password');
+  }
+  if (password === 'secret') {
+    return 'welcome!!!';
+  }
 
-const sing = async () => {
-  throw new Error('error occered');
-  return 'hogehoge';
+  throw new Error('password is wrong');
 };
 
-sing()
-  .then(data => {
-    console.log('success', data);
+// login('hoge', 'hogehoge')
+login('hoge', 'secret')
+  .then(msg => {
+    console.log('login success', msg);
   })
   .catch(err => {
-    console.log('failed', err);
+    console.log('login error', err);
   });
+
+// async function hello() {}
+
+// const sing = async () => {
+//   throw new Error('error occered');
+//   return 'hogehoge';
+// };
+
+// sing()
+//   .then(data => {
+//     console.log('success', data);
+//   })
+//   .catch(err => {
+//     console.log('failed', err);
+//   });
