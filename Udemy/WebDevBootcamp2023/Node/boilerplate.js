7ccbf411c0030ee65f760c85826d9005c62c3c97
SI <>
2025-12-09 11:17:07 +0900

    feat: node mkdir and file

--- Udemy/WebDevBootcamp2023/Node/boilerplate.js
+++ Udemy/WebDevBootcamp2023/Node/boilerplate.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
+const dirName = process.argv[2] || 'Project';
 // console.log(fs);
 
-fs.mkdir('Dogs', { recursive: true }, err => {
-  console.log('in callback');
-  if (err) throw err;
-});
+// fs.mkdir('Dogs', { recursive: true }, err => {
+//   console.log('in callback');
+//   if (err) throw err;
+// });
 
-console.log('this is the most below place');
+// fs.mkdirSync('Cats');
+
+try {
+  fs.mkdirSync(dirName);
+  fs.writeFileSync(`${dirName}/index.html`, '');
+  fs.writeFileSync(`${dirName}/app.js`, '');
+  fs.writeFileSync(`${dirName}/style.css`, '');
+} catch (e) {
+  console.log('error occured!!!', e);
+}
+
+// console.log('this is the most below place');
