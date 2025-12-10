337717b05f93a7e440bb66ab1c8568132d01a86e
pirosiki002 <considerate.growth.family@gmail.com>
2025-12-11 07:00:56 +0900

    feat: app.get request

--- Udemy/WebDevBootcamp2023/Express/FirstApp/index.js
+++ Udemy/WebDevBootcamp2023/Express/FirstApp/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-  console.log('i received your request');
-  //   console.dir(req);
-  //   res.send('ill respond req to you');
-  //   res.send({ color: 'red' });
+// app.use((req, res) => {
+//   console.log('i received your request');
+//   //   console.dir(req);
+//   //   res.send('ill respond req to you');
+//   //   res.send({ color: 'red' });
+//   res.send('<h1>my first Web page</h1>');
+// });
+
+app.get('/cats', (req, res) => {
+  console.log(`i received URL of /cats`);
+  res.send('meow');
+});
+
+app.post('/cats', (req, res) => {
+  res.send('i received post request of /cats');
+});
+
+app.get(`/dogs`, (req, res) => {
+  console.log(`i received URL of /dogs`);
+  res.send('bow wow');
+});
+
+app.get('/', (req, res) => {
+  console.log('here is HP');
+  res.send('HP home');
+});
+
+// order is important
+app.get(/(.*)/, (req, res) => {
+  res.send('unknown pass');
 });
 
 app.listen(8080, () => {
