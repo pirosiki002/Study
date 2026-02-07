644d38cf651ee7a1ab9bdccee82d672649f62c34
SI <>
2026-02-07 09:15:02 +0900

    feat: connect MongoDB with Mongoose

--- Udemy/WebDevBootcamp2023/MongooseBasics/index.js
+++ Udemy/WebDevBootcamp2023/MongooseBasics/index.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:207017/movieApp', {
+mongoose
+  .connect('mongodb://localhost:27017/movieApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('connection OK!!!!');
+  })
+  .catch(err => {
+    console.log('connection Error!!!!!!!!');
+    console.log(err);
+  });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-  // we're connected!!!!
-  console.log('we are connected!!!');
-});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   // we're connected!!!!
+//   console.log('we are connected!!!');
+// });
