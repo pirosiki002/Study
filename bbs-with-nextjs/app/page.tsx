d25a8ff3068f8bdb763c339d30e38f3753dfeedb
SI <>
2026-03-03 11:06:35 +0900

    refactor: create component of the BBSCardList

--- bbs-with-nextjs/app/page.tsx
+++ bbs-with-nextjs/app/page.tsx
@@ -1,78 +1,12 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import Link from 'next/link';
-import Image from 'next/image';
+import BBSCardList from './components/BBSCardList';
 // import { Card } from '@/components/ui/card';
 
 export default function Home() {
   return (
     // added bg-white because bg == black
-    <main className="grid lg:grid-cols-3 px-4 py-4 gap-4">
+    <main>
       {/* ui Card https://ui.shadcn.com/docs/components/radix/card */}
-      {/* <Card /> */}
-      <Card>
-        <CardHeader>
-          <CardTitle>Create Project</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
-          omnis, accusantium velit sunt ipsa libero, magnam earum facilis
-          assumenda temporibus veritatis in possimus? Tenetur maxime cumque
-          nihil assumenda! Commodi, culpa!
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Link href={'/bbs-posts/1'} className="text-blue-500">
-            Read more
-          </Link>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create Project</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
-          omnis, accusantium velit sunt ipsa libero, magnam earum facilis
-          assumenda temporibus veritatis in possimus? Tenetur maxime cumque
-          nihil assumenda! Commodi, culpa!
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Link href={'/bbs-posts/1'} className="text-blue-500">
-            Read more
-          </Link>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create Project</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
-          omnis, accusantium velit sunt ipsa libero, magnam earum facilis
-          assumenda temporibus veritatis in possimus? Tenetur maxime cumque
-          nihil assumenda! Commodi, culpa!
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Link href={'/bbs-posts/1'} className="text-blue-500">
-            Read more
-          </Link>
-        </CardFooter>
-      </Card>
+      <BBSCardList />
     </main>
   );
 }
