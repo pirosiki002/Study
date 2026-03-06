5e0df45c71ee0a9717970eb4b41e8f3e8693782c
SI <>
2026-03-07 08:29:16 +0900

    feat: useRef base

---
+++ nextjs-dashboard/app/useRef/page.client.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { FC } from 'react';
+
+const ClientPage: FC = () => {
+  return <>This is the test of the useRef</>;
+};
+
+export default ClientPage;
---
+++ nextjs-dashboard/app/useRef/page.tsx
@@ -0,0 +1,4 @@
+import ClientPage from './page.client';
+export default function UseRef() {
+  return <ClientPage />;
+}
