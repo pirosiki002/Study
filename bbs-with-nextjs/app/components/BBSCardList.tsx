8c01269d6c8dc535bf5211ada395e45fd2378ade
SI <>
2026-03-24 09:09:25 +0900

    feat: send api data to CardList

--- bbs-with-nextjs/app/components/BBSCardList.tsx
+++ bbs-with-nextjs/app/components/BBSCardList.tsx
@@ -1,6 +1,13 @@
 import BBSCard from './BBSCard';
+import { BBSData } from '@/app/types/types';
 
-export default function BBSCardList() {
+interface BBSDataProps {
+  bbsAllData: BBSData;
+}
+
+// export default function BBSCardList({ bbsAllData }: BBSDataProps) {
+export default function BBSCardList(props: BBSDataProps) {
+  const { bbsAllData } = props;
   return (
     <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
       <BBSCard />
--- bbs-with-nextjs/app/page.tsx
+++ bbs-with-nextjs/app/page.tsx
@@ -1,19 +1,25 @@
-import { PrismaClient } from '@prisma/client/extension';
+// import { PrismaClient } from '@prisma/client/extension';
 import BBSCardList from './components/BBSCardList';
+import { BBSData } from '@/app/types/types';
 
-export default async function Home() {
+async function getBBSAllData() {
   const response = await fetch('http://localhost:3000/api/post', {
     cache: 'no-store',
   });
 
-  const bbsAllData = await response.json();
+  const bbsAllData: BBSData = await response.json();
+  return bbsAllData;
+}
+
+export default async function Home() {
+  const bbsAllData = await getBBSAllData();
   console.log(bbsAllData);
 
   return (
     // added bg-white because bg == black
     <main>
       {/* ui Card https://ui.shadcn.com/docs/components/radix/card */}
-      <BBSCardList />
+      <BBSCardList bbsAllData={bbsAllData} />
     </main>
   );
 }
