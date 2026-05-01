2dc42d6a4ca06f239e66af434a48be6f9c3eb5ff
pirosiki002 <considerate.growth.family@gmail.com>
2026-05-02 06:15:56 +0900

    feat: create form

---
+++ bbs-with-nextjs/app/bbs-posts/create/page.tsx
@@ -0,0 +1,101 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Textarea } from '@/components/ui/textarea';
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: 'please input over 2 letters about username' }),
+  title: z
+    .string()
+    .min(2, { message: 'please input over 2 letters about title' }),
+  content: z
+    .string()
+    .min(10, { message: 'please input over 10 letters about main contents' })
+    .max(140, {
+      message: 'please input within 140 letter about main contents',
+    }),
+});
+
+const CreatBBSPage = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      title: '',
+      content: '',
+    },
+  });
+
+  async function onSubmit() {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-3 w-1/2 px-7"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>title</FormLabel>
+              <FormControl>
+                <Input placeholder="title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>content</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="content"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CreatBBSPage;
