6022f594c4628656ad0d866bdf1135d96eb28168
SI <>
2026-07-07 08:57:08 +0900

    feat: useState setAge *3

--- react-hooks-tutorial/src/lessons/Lesson1/Lesson1_1.tsx
+++ react-hooks-tutorial/src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,32 @@
+'use client';
+import { useState } from 'react';
+
 const Lesson1_1 = () => {
+  // const [age, setAge] = useState(0);
+  const [age, setAge] = useState<number>(0);
+
+  const handleClick = () => {
+    setAge(age + 1);
+    console.log(age);
+    // 更新されていないage 0に１を加える
+    setAge(age + 1);
+    console.log(age);
+    // 更新されていないage 0に１を加える
+    setAge(age + 1);
+    console.log(age);
+    // すべての処理が終わったら再レンダリングで値を反映する
+  };
+
   return (
     <div>
       <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <button
+        className="border p-2 rounded-md bg-red-100"
+        onClick={handleClick}
+      >
+        Add Age
+      </button>
+      <p>You are {age}</p>
     </div>
   );
 };
