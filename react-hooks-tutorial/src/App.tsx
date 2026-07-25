86f21003eb6e2d0c339219250e0f49b44aaeff0f
SI <>
2026-07-25 09:27:20 +0900

    feat: useEffect part1

--- react-hooks-tutorial/src/App.tsx
+++ react-hooks-tutorial/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import Lesson2_1 from './lessons/Lesson2/Lesson2_1/Lesson2_1';
+// import Lesson2_1 from './lessons/Lesson2/Lesson2_1/Lesson2_1';
+import Lesson2_2 from './lessons/Lesson2/Lesson2_2/Lesson2_2';
 // import Lesson1_2 from './lessons/Lesson1/Lesson1_2';
 // import Lesson1_1 from './lessons/Lesson1/Lesson1_1';
 
@@ -8,7 +9,7 @@ function App() {
     <div className="flex items-center max-w-4xl mx-auto py-8 text-2xl">
       {/* <Lesson1_1 /> */}
       {/* <Lesson1_2 /> */}
-      <Lesson2_1 />
+      <Lesson2_2 />
       {/* <Lesson3_1 /> */}
       {/* ... */}
     </div>
--- react-hooks-tutorial/src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
+++ react-hooks-tutorial/src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
@@ -1,7 +1,21 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { fetchBio } from './fetchBio';
+
 const Lesson2_2 = () => {
+  const [person, setPerson] = useState<string>('ShinCode');
+
+  useEffect(() => {
+    const startFetching = async () => {
+      const response = await fetchBio(person);
+      console.log(response);
+    };
+    startFetching();
+  }, []);
+
   return (
     <div>
-      <select>
+      <select onChange={e => setPerson(e.target.value)} value={person}>
         <option value="ShinCode">ShinCode</option>
         <option value="TestUser">TestUser</option>
         <option value="SampleUser">SampleUser</option>
@@ -9,7 +23,7 @@ const Lesson2_2 = () => {
 
       <hr />
 
-      <p className="text-black">{"Loading..."}</p>
+      <p className="text-black">{'Loading...'}</p>
     </div>
   );
 };
