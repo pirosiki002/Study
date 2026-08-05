0bd8a195975a6e312d08b40b0c4962d9d813a19b
SI <>
2026-08-06 08:40:08 +0900

    feat: useRef DOM control

--- react-hooks-tutorial/src/App.tsx
+++ react-hooks-tutorial/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 // import Lesson2_3 from './lessons/Lesson2/Lesson2_3/Lesson2_3';
 // import Lesson1_2 from './lessons/Lesson1/Lesson1_2';
 // import Lesson1_1 from './lessons/Lesson1/Lesson1_1';
-import Lesson3_1 from './lessons/Lesson3/Lesson3_1';
+import Lesson3_2 from './lessons/Lesson3/Lesson3_2';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       {/* <Lesson1_2 /> */}
       {/* <Lesson2_2 /> */}
       {/* <Lesson2_3 /> */}
-      <Lesson3_1 />
+      <Lesson3_2 />
       {/* ... */}
     </div>
   );
