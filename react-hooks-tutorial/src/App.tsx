3c8805dee6984ee260213e4f0bd12ae08b5867d9
SI <>
2026-08-08 08:34:39 +0900

    feat: useRef forwardRef

--- react-hooks-tutorial/src/App.tsx
+++ react-hooks-tutorial/src/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 // import Lesson1_2 from './lessons/Lesson1/Lesson1_2';
 // import Lesson1_1 from './lessons/Lesson1/Lesson1_1';
 // import Lesson3_2 from './lessons/Lesson3/Lesson3_2';
-import Lesson3_3 from './lessons/Lesson3/Lesson3_3';
+// import Lesson3_3 from './lessons/Lesson3/Lesson3_3';
+import Lesson3_4 from './lessons/Lesson3/Lesson3_4/Lesson3_4';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       {/* <Lesson2_2 /> */}
       {/* <Lesson2_3 /> */}
       {/* <Lesson3_2 /> */}
-      <Lesson3_3 />
+      {/* <Lesson3_3 /> */}
+      <Lesson3_4 />
       {/* ... */}
     </div>
   );
