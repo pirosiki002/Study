21e2f644be9f889b0c44e3357878030962603c37
SI <>
2026-08-08 08:12:44 +0900

    feat: useRef rerendering stop

--- react-hooks-tutorial/src/App.tsx
+++ react-hooks-tutorial/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 // import Lesson2_3 from './lessons/Lesson2/Lesson2_3/Lesson2_3';
 // import Lesson1_2 from './lessons/Lesson1/Lesson1_2';
 // import Lesson1_1 from './lessons/Lesson1/Lesson1_1';
-import Lesson3_2 from './lessons/Lesson3/Lesson3_2';
+// import Lesson3_2 from './lessons/Lesson3/Lesson3_2';
+import Lesson3_3 from './lessons/Lesson3/Lesson3_3';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Lesson1_2 /> */}
       {/* <Lesson2_2 /> */}
       {/* <Lesson2_3 /> */}
-      <Lesson3_2 />
+      {/* <Lesson3_2 /> */}
+      <Lesson3_3 />
       {/* ... */}
     </div>
   );
--- react-hooks-tutorial/src/lessons/Lesson3/Lesson3_3.tsx
+++ react-hooks-tutorial/src/lessons/Lesson3/Lesson3_3.tsx
@@ -1,9 +1,26 @@
+import { useRef } from 'react';
+
 const Lesson3_3 = () => {
-  const handleClick = () => {};
+  // const [inputText, setInputText] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    // alert(inputText);
+    alert(inputRef.current?.value);
+  };
+
+  console.log('rendered');
 
   return (
     <div>
-      <input type="text" className="border-b" />
+      <input
+        type="text"
+        className="border-b"
+        // onChange={e => {
+        //   setInputText(e.target.value);
+        // }}
+        ref={inputRef}
+      />
       <button onClick={handleClick}>input入力値を見る</button>
     </div>
   );
